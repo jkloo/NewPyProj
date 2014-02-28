4d4a0286672c7af31b3a13bea94ef497ac5ba163
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-02-28 01:32:56 -0500

    modified setup.py from template

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ Setup script for NewPyProj.
 
 import setuptools
 
-from npp import __project__, __version__
+from npp import __project__, __version__, CLI
 
 import os
 if os.path.exists('README.rst'):
@@ -20,14 +20,14 @@ setuptools.setup(
     name=__project__,
     version=__version__,
 
-    description="NewPyProj is a Python 3 package template.",
+    description="NewPyProj is a utility for creating a Python 3 package from a template.",
     url='http://pypi.python.org/pypi/NewPyProj',
-    author='Jace Browning',
-    author_email='jacebrowning@gmail.com',
+    author='Jeff Kloosterman',
+    author_email='kloosterman.jeff@gmail.com',
 
     packages=setuptools.find_packages(),
 
-    entry_points={'console_scripts': []},
+    entry_points={'console_scripts': [CLI + ' = npp.cli:main']},
 
     long_description=(README + '\n' + CHANGES),
     license='WTFPL',
