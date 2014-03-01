2c985c4b9a5189d2e5b3d20c28fa6d15e9923690
Jeff Kloosterman <kloosterman.jeff@gmail.com>
2014-02-28 19:08:04 -0500

    forgot py extension

--- npp/test/test_gitactions.py
+++ npp/test/test_gitactions.py
@@ -10,82 +10,92 @@ from npp.gitactions import git_merge_from_remote, init_git_repo
 class TestGetGitEmail(unittest.TestCase):
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__get_git_email__no_email(self):
-        self.assertEqual(_get_git_email(), '')
+        self.assertEqual(get_git_email(), '')
 
     @patch('subprocess.check_call', Mock(return_value='jeff@email.com'))
     def test__get_git_email__valid_email(self):
-        self.assertEqual(_get_git_email(), 'jeff@email.com')
+        self.assertEqual(get_git_email(), 'jeff@email.com')
 
     @unittest.skip('Need to add email address validation')
     @patch('subprocess.check_call', Mock(return_value='this is not an email'))
     def test__get_git_email__invalid_email(self):
-        self.assertEqual(_get_git_email(), '')
+        self.assertEqual(get_git_email(), '')
 
 
 class TestGetGitUsername(unittest.TestCase):
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__get_git_username__no_username(self):
-        self.assertEqual(_get_git_username(), '')
+        self.assertEqual(get_git_username(), '')
 
     @patch('subprocess.check_call', Mock(return_value='Jeff Kloosterman'))
     def test__get_git_username__valid_username(self):
-        self.assertEqual(_get_git_username(), 'Jeff Kloosterman')
+        self.assertEqual(get_git_username(), 'Jeff Kloosterman')
 
 
 class TestInitGitRepo(unittest.TestCase):
     @patch('subprocess.check_call', Mock(return_value='Initialized empty Git repository in /path/to/git/NewPyProj/.git/'))
     def test__init_git_repo__success(self):
-        self.assertTrue(_init_git_repo())
+        self.assertTrue(init_git_repo())
 
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__init_git_repo__failure(self):
-        self.assertFalse(_init_git_repo())
+        self.assertFalse(init_git_repo())
 
 
 class TestAddRepoRemote(unittest.TestCase):
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))
     def test__add_repo_remote__success(self):
-        self.assertTrue(_add_repo_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertTrue(add_repo_remote('testing', 'https://testurl.com/repo.git'))
 
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__add_repo_remote__failure(self):
-        self.assertFalse(_add_repo_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertFalse(add_repo_remote('testing', 'https://testurl.com/repo.git'))
+
+
+class TestSetRepoRemote(unittest.TestCase):
+    @patch('subprocess.check_call', Mock(return_value='some message about remote added'))
+    def test__set_repo_remote__success(self):
+        self.assertTrue(set_repo_remote('testing', 'https://testurl.com/repo.git'))
+
+    @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
+    def test__set_repo_remote__failure(self):
+        self.assertFalse(set_repo_remote('testing', 'https://testurl.com/repo.git'))
 
 
 class TestGitPullFromRemote(unittest.TestCase):
-    @patch('npp.gitactions._git_fetch_from_remote', Mock(return_value=True))
-    @patch('npp.gitactions._git_merge_from_remote', Mock(return_value=True))
+    @patch('npp.gitactions.git_fetch_from_remote', Mock(return_value=True))
+    @patch('npp.gitactions.git_merge_from_remote', Mock(return_value=True))
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))    
     def test__git_pull_from_remote__success(self):
-        self.assertTrue(_git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertTrue(git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
 
-    @patch('npp.gitactions._git_fetch_from_remote', Mock(return_value=False))
+    @patch('npp.gitactions.git_fetch_from_remote', Mock(return_value=False))
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))
     def test__git_pull_from_remote__failure_fetch(self):
-        self.assertFalse(_git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertFalse(git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
 
-    @patch('npp.gitactions._git_merge_from_remote', Mock(return_value=False))
+    @patch('npp.gitactions.git_merge_from_remote', Mock(return_value=False))
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))
     def test__git_pull_from_remote__failure_merge(self):
-        self.assertFalse(_git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertFalse(git_pull_from_remote('testing', 'https://testurl.com/repo.git'))
 
 
 class TestGitFetchFromRemote(unittest.TestCase):
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))    
     def test__git_fetch_from_remote__success(self):
-        self.assertTrue(_git_fetch_from_remote('testing'))
+        self.assertTrue(git_fetch_from_remote('testing'))
 
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__git_fetch_from_remote__failure_merge(self):
-        self.assertFalse(_git_fetch_from_remote('testing'))
+        self.assertFalse(git_fetch_from_remote('testing'))
 
 
 class TestGitMergeFromRemote(unittest.TestCase):
     @patch('subprocess.check_call', Mock(return_value='some message about remote added'))    
     def test__git_merge_from_remote__success(self):
-        self.assertTrue(_git_merge_from_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertTrue(git_merge_from_remote('testing', 'https://testurl.com/repo.git'))
 
     @patch('subprocess.check_call', Mock(side_effect=CalledProcessError('', '')))
     def test__git_merge_from_remote__failure_merge(self):
-        self.assertFalse(_git_merge_from_remote('testing', 'https://testurl.com/repo.git'))
+        self.assertFalse(git_merge_from_remote('testing', 'https://testurl.com/repo.git'))
 
